bda971af3f53dad4a4a61ef947062b489c22a039
RenaissanceZwy <zhaowenyi96@163.com>
2016-03-18 08:54:19 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//定义项目文件
+define('APP_NAME','cog');
+define('APP_PATH','./cog/');
+define('APP_DEBUG','true');
+
+// 定义前台路径
+define('URL','/cog/index.php/Home/');
+//定义前台路径
+define('HOME_CSS','/cog/cog/Public/css/');
+define('HOME_JS','/cog/cog/Public/js/');
+define('HOME_IMGS','/cog/cog/Public/imgs/');
+
+require './ThinkPHP/ThinkPHP.php';
